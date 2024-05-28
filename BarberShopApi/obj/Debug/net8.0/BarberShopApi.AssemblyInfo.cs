bff1bb173dcf851ca119c0a4add165aebe2b1c86
maayansaid <maayansaid1234@gmail.com>
2024-05-28 22:15:43 +0300

    move dockerfile to solution

--- BarberShopApi/obj/Debug/net8.0/BarberShopApi.AssemblyInfo.cs
+++ BarberShopApi/obj/Debug/net8.0/BarberShopApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarberShopApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+880f91df41bbc2bf7098279e0dbb68de0bb969c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb296373006ae63f0ce3bd0abc21c516f5152d66")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarberShopApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarberShopApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
